bd68527e5d7569b73e4c4a14a99257d0bf7a1287
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2018-10-13 14:47:24 +0300

    First commit
    snappyHexMesh stil delete injection patches

---
+++ geometry/scaleSTL.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Масштабирование файлов для последующего
+
+# ------------------------------------ Data -------------------------------- #
+
+# Указание пути к файлам .stl для их копирования
+cp -r CATIA/*.stl ./
+
+# -------------------------------------------------------------------------- #
+
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Script ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] #
+
+# Inlet
+export FILE="injectionXMinus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="injectionXPlus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="injectionYMinus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="injectionYPlus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+
+# Injection
+export FILE="inletXMinus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="inletXPlus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="inletYMinus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="inletYPlus.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+
+# Other
+export FILE="outlet.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="valve.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+export FILE="valve.stl"
+surfaceTransformPoints -translate '(0 0 -0.25)' $FILE $FILE
+export FILE="walls.stl"
+surfaceTransformPoints -scale '(0.001 0.001 0.001)' $FILE $FILE
+
+
+
+
+
