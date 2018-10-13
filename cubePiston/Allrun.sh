469104b504d5677a6902d16c38f516542d9078a4
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2018-10-13 18:26:30 +0300

    Works with renamed solver

---
+++ cubePiston/Allrun.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# ------------------------------------ Data -------------------------------- #
+
+# Указать путь к файлам .stl в скрипте scaleSTL.sh для их последующего 
+# масштабирования в geometry/
+
+
+# -------------------------------------------------------------------------- #
+
+
+
+# [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ Script ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] #
+
+startProjectTime=`date +%s` # Включение секундомера для вывода времени расчёта
+
+
+## Масштабирование файлов .stl
+printf 'Scaling .stl files...'
+cd geometry
+sh scaleSTL.sh > scaleSTL.log 
+printf '\nScaling *.stl files has being DONE.'
+printf '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
+
+## Построение сетки
+printf 'Meshing...'
+cd ../mesh
+sh firstMesh.sh > mesh.log 
+printf '\nThe mesh has being DONE.'
+printf '\n~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
+
+## Решение проекта
+printf 'Solving the case...'
+cd ../case
+# sh firstRun.sh > case.log
+sh rerun.sh > case.log
+printf '\nThe case has being SOLVED.'
+printf '\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
+
+## Вывод времени расчёта проекта в терминал и сигнал о завершении расчёта
+cd ../
+endProjectTime=`date +%s`
+solveProjectTime=$((endProjectTime-startProjectTime))
+printf '#######################\n'
+printf 'Solve time: %dh:%dm:%ds\n' $(($solveProjectTime/3600)) $(($solveProjectTime%3600/60)) $(($solveProjectTime%60))
+echo -ne '\007' # звуковой сигнал
+
