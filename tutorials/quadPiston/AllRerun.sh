f8b77b074612f745d003936114d172275682ac8d
Stanislau <stas.dmitrievich.stashevskiy@gmail.com>
2019-05-26 22:58:14 +0300

    Move to the Allrun & the Allclean scripts

--- tutorials/quadPiston/AllRerun.sh
+++ tutorials/quadPiston/AllRerun.sh
@@ -7,7 +7,7 @@
 #    \\/     M anipulation  |
 #------------------------------------------------------------------------------
 # Script
-#     Allrun
+#     AllRerun
 #
 # Description
 #     Generates mesh and run all cases using the multiCompression solver
@@ -16,44 +16,41 @@
 
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
 
+cd ${0%/*} || exit 1 # Run from this directory
+
 startProjectTime=`date +%s` # Включение секундомера для вывода времени расчёта
 
 # Масштабирование файлов .stl
 cd geometry
-printf 'Running surfaceTransformPoints (scaling .stl files) on %s\n' "${PWD}"
-sh scaleSTL.sh > scaleSTL.log
+./Allclean && ./Allrun
+printf '\n'
 
 # Построение сетки
 cd ../mesh
-printf 'Running snappyHexMesh on %s\n' "${PWD}"
-sh remesh.sh > mesh.log
+./Allclean && ./Allrun
+printf '\n'
 
 # Решение проекта на собственном ядре
 cd ../case
-printf 'Running multiCompression on %s\n' "${PWD}"
-# sh firstRun.sh > case.log
-sh hardResolve.sh > case.log
+./Allclean && ./Allrun
+printf '\n'
 
 # Решение проекта для сравнения результатов c potentialFoam'ом
 cd ../casePotential
-printf 'Running potentialFoam on %s\n' "${PWD}"
-# sh firstRun.sh > case.log
-sh hardResolve.sh > case.log
+./Allclean && ./Allrun
+printf '\n'
 
 # Решение проекта для сравнения результатов со scalarTransportFoam'ом
 cd ../caseScalar
-printf 'Running scalarTransportFoam on %s\n' "${PWD}"
-# sh firstRun.sh > case.log
-sh hardResolve.sh > case.log
+./Allclean && ./Allrun
+printf '\n'
 
 # Отображение времени расчёта проекта
 cd ../
 endProjectTime=`date +%s`
 solveProjectTime=$((endProjectTime-startProjectTime))
-printf '\nCalculation time: %dh:%dm:%ds\n'\
+printf 'Calculation time: %dh:%dm:%ds\n'\
 	 $(($solveProjectTime/3600)) $(($solveProjectTime%3600/60)) $(($solveProjectTime%60))
 
 echo -ne '\007' # звуковой сигнал
-
 # ***************************************************************************** #
-
