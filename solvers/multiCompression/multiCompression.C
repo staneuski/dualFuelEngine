d4aed61a13e0ce0f3b8da2bdb0c97f23a613cbe5
StasF1@macOS <stas.dmitrievich.stashevskiy@gmail.com>
2019-07-09 21:18:40 +0300

    [s/] correctNonOrthogonal loop deleted

--- solvers/multiCompression/multiCompression.C
+++ solvers/multiCompression/multiCompression.C
@@ -1,37 +1,37 @@
 /*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: Addition to OpenFOAM v6
-   \\    /   O peration     | Website:  https://github.com/StasF1/dualFuelEngine
-    \\  /    A nd           | Copyright (C) 2019 Stanislau Stasheuski
-     \\/     M anipulation  |
+  =========					|
+  \\	  /	 F ield			| OpenFOAM: Addition to OpenFOAM v6
+   \\	 /	 O peration		| Website:	https://github.com/StasF1/dualFuelEngine
+	\\	/	 A nd			| Copyright (C) 2019 Stanislau Stasheuski
+	 \\/	 M anipulation	|
 -------------------------------------------------------------------------------
 License
-    This file is not part of OpenFOAM, but part of dualFuelEngine – OpenFOAM
-    addition.
+	This file is not part of OpenFOAM, but part of dualFuelEngine – OpenFOAM
+	addition.
 
-    dualFuelEngine (like OpenFOAM) is free software: you can redistribute it 
-    and/or modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the License,
-    or (at your option) any later version.
+	dualFuelEngine (like OpenFOAM) is free software: you can redistribute it 
+	and/or modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the License,
+	or (at your option) any later version.
 
-    dualFuelEngine (like OpenFOAM) is distributed in the hope that it will be
-    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU General Public License for more details.
+	dualFuelEngine (like OpenFOAM) is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this repository. If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this repository. If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    multiCompression
+	multiCompression
 
 Description
-    v0.2-alpha
+	v0.2-alpha
 
 Comments
 
-    fvc - явный метод,   возвращает поле
-    fvm - неявный метод, возвращает контрольно-объёмную матрицу
+	fvc - явный метод,	 возвращает поле
+	fvm - неявный метод, возвращает контрольно-объёмную матрицу
 
 \*---------------------------------------------------------------------------*/
 
@@ -43,92 +43,88 @@ Comments
 
 int main(int argc, char *argv[])
 {
-    #include "setRootCaseLists.H"
-    #include "createTime.H"
-    #include "createMesh.H"
+	#include "setRootCaseLists.H"
+	#include "createTime.H"
+	#include "createMesh.H"
 
-    simpleControl simple(mesh);
+	simpleControl simple(mesh);
 
-    #include "createFields.H"
+	#include "createFields.H"
 
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-    
-    #include "CourantNo.H"
-    
-    Info<< "\nStarting time loop\n" << endl;
-    
-    while (simple.loop(runTime))
-    {
-        Info<< "Time = " << runTime.timeName() << nl << endl;
-        
-        while (simple.correctNonOrthogonal())
-        {
-			
-            fvScalarMatrix TEqn
-            (
-                fvm::ddt(rho, T)
-              + fvm::div(phi, T)
-              - fvc::laplacian(DT, rho*T)
-            );
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+	
+	#include "CourantNo.H"
+	
+	Info<< "\nStarting time loop\n" << endl;
+	
+	while (simple.loop(runTime))
+	{
+		Info<< "Time = " << runTime.timeName() << nl << endl;
 
-            // TEqn.relax(); //FIXME What are these lines (76 & 79) mean ?
-            fvOptions.constrain(TEqn);
-            TEqn.solve();
-            // fvOptions.correct(T);
-            
-            fvScalarMatrix rhoEqn
-            (
-                fvm::ddt(rho)
-              + fvc::div(phi)
-            );
+		
+		fvScalarMatrix TEqn
+		(
+			fvm::ddt(rho, T)
+		  + fvm::div(phi, T)
+		  - fvc::laplacian(DT, rho*T)
+		);
 
-            rhoEqn.relax();
-            rhoEqn.solve();
-           
-            fvVectorMatrix UEqn
-            (
-                fvm::ddt(rho, U)
-              + fvc::div(phi, U)
-             ==
-              - fvc::grad(p)
-              + fvc::grad
-                (
-                    (MU/3)*fvc::div(U)
-                )
-              + MU*fvc::laplacian(U)
-            );
+		// TEqn.relax(); //FIXME What are these lines (76 & 79) mean ?
+		fvOptions.constrain(TEqn);
+		TEqn.solve();
+		// fvOptions.correct(T);
+		
+		fvScalarMatrix rhoEqn
+		(
+			fvm::ddt(rho)
+		  + fvc::div(phi)
+		);
 
-            UEqn.relax();
-            UEqn.solve();
+		rhoEqn.relax();
+		rhoEqn.solve();
+	
+		fvVectorMatrix UEqn
+		(
+			fvm::ddt(rho, U)
+		  + fvc::div(phi, U)
+		 ==
+		  - fvc::grad(p)
+		  + fvc::grad
+			(
+				(MU/3)*fvc::div(U)
+			)
+		  + MU*fvc::laplacian(U)
+		);
 
-            fvScalarMatrix eEqn
-            (
-                fvm::ddt(rho, e)
-			  + fvc::div(phi, e)
-             ==
-              - fvc::div(p*U)
-              + fvc::div(LAMBDA*fvc::grad(T))
-              + ( U & (MU*fvc::laplacian(U) + fvc::grad(MU/3*fvc::div(U))) )
-            // TODO Add mu*D
-            );
+		UEqn.relax();
+		UEqn.solve();
 
-            eEqn.relax();
-            eEqn.solve();
-			
-			phi = fvc::flux(rho*U);
-			
-			p = rho*(GAMMA - 1)*(e - magSqr(U)/2);
-            
-        }
-        
-        runTime.write();
+		fvScalarMatrix eEqn
+		(
+			fvm::ddt(rho, e)
+		  + fvc::div(phi, e)
+		 ==
+		  - fvc::div(p*U)
+		  + fvc::div(LAMBDA*fvc::grad(T))
+		  + ( U & (MU*fvc::laplacian(U) + fvc::grad(MU/3*fvc::div(U))) )
+		// TODO Add mu*D
+		);
 
-        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
-    }
+		eEqn.relax();
+		eEqn.solve();
+		
+		phi = fvc::flux(rho*U);
+		
+		p = rho*(GAMMA - 1)*(e - magSqr(U)/2);
+		
+		runTime.write();
 
-    Info<< "End\n" << endl;
+		Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+			<< "  ClockTime = " << runTime.elapsedClockTime() << " s"
+			<< nl << endl;
+	}
+
+	Info<< "End\n" << endl;
 
 }
 
