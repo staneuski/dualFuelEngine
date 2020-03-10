80945ca8aed69ef5427d18657e868360d2791c05
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2020-03-10 18:51:22 +0300

    Edit dict & reportGenerator at etc/DRK2py/

--- etc/DRK2Py/DRK2PyDict.py
+++ etc/DRK2Py/DRK2PyDict.py
@@ -8,30 +8,9 @@
 
 from math import pi
 
-# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 tmpFolder          = 'tmp*' # path to Diesel-RK results folder
-
-n                  = 92 # RPM
-
-S                  = 2.7 # m
-
-IPO                = 42 # ˚CA before BDC
-
-IPC                = IPO # ˚CA after BDC
-
-EVO                = 85 # ˚CA before BDC
-
-EVC                = 46 # ˚CA after BDC
-
-inletArea          = 1.092e-03 # m^2
-
-
-injG_max           = 1.17 # kg/s, q_injGas = 95 (g), t_injGas = 0.0815 (s))
-
-injCA2Max          = 10 # ˚CA
-
-injArea            = pi*pow(0.85e-03, 2)/4*2 # m^2, area of the two injector orifices d = 0.85 (mm)
 
 
 terminalOutput     = 'true' # true or false
@@ -46,7 +25,35 @@ movingPartsPlot    = 'false' # true or false
 
 inletInjectionPlot = 'false' # true or false
 
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-# *************************************************************************** #
+n                  = 92 # RPM
 
-# R = 518.3 # J/mol/K specific gas constant
+S                  = 2.7 # m
+
+
+IPO                = 42 # ˚CA before BDC
+
+IPC                = IPO # ˚CA after BDC
+
+EVO                = 85 # ˚CA before BDC
+
+EVC                = 46 # ˚CA after BDC
+
+inletArea          = 1.092e-03 # m^2
+
+
+injG_max           = 1 # kg/s, q_injGas = 95 (g), t_injGas = 0.0815 (s)
+
+injCA2Max          = 10 # ˚CA
+
+injArea            = pi*pow(3.75e-03, 2)/4*2 # m^2, area which equals of the two injector orifices d = 3.75 (mm)
+
+injT               = 380 # K (estimated)
+
+injP               = 150e+05 # Pa (estimated)
+
+injR               = 518.3 # J/mol/K specific gas constant
+
+
+# *****************************************************************************
