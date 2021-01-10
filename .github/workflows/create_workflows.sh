209ff758b323b06a5e241cd46281516310558c1c
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-01-10 12:40:48 +0300

    Use only main.yml workflow with matrix.case strategy

--- .github/workflows/create_workflows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ${0%/*} || exit 1    # Run from this directory
-
-solvers=("cylCyclic2D" "pipeCompression" "tubePurging")
-
-for solver in "${solvers[@]}"; do
-    sed "s/shockTube/$solver/g" shockTube.yml > $solver.yml
-done
